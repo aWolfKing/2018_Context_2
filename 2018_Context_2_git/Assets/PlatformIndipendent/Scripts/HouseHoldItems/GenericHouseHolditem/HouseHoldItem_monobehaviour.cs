3c122089fba407acbe15f14ec6d36e4519725bea
aWolfKing <31818613+aWolfKing@users.noreply.github.com>
2018-03-19 17:44:49 +0100

    Placement colors

--- 2018_Context_2_git/Assets/PlatformIndipendent/Scripts/HouseHoldItems/GenericHouseHolditem/HouseHoldItem_monobehaviour.cs
+++ 2018_Context_2_git/Assets/PlatformIndipendent/Scripts/HouseHoldItems/GenericHouseHolditem/HouseHoldItem_monobehaviour.cs
@@ -8,6 +8,7 @@ public class HouseHoldItem_monobehaviour : MonoBehaviour {
         canPlace, cannotPlace, normalMaterial
     }
     public VisualMaterial visualMaterial = VisualMaterial.normalMaterial;
+    private VisualMaterial visualMaterial_was = VisualMaterial.normalMaterial;
 
     private float influencePercent = 0;
     [System.NonSerialized] private HouseHoldItemData houseHoldItemData = null;
@@ -20,12 +21,49 @@ public class HouseHoldItem_monobehaviour : MonoBehaviour {
         }
     }
 
+    private static Material canPlaceMaterial = null;
+    private static Material cannotPlaceMaterial = null;
+    private Dictionary<MeshRenderer, Material[]> originalMaterials = new Dictionary<MeshRenderer, Material[]>();
+
+    [SerializeField] private Material m_canPlaceMaterial = null;
+    [SerializeField] private Material m_cannotPlaceMaterial = null;
+
+
+
     private void OnEnable() {
-        MainGameManager.AddObject(this);
+
+        if(canPlaceMaterial == null && m_canPlaceMaterial != null){ canPlaceMaterial = m_canPlaceMaterial; }
+        if(cannotPlaceMaterial == null && m_cannotPlaceMaterial != null){ cannotPlaceMaterial = m_cannotPlaceMaterial; }
+
+        if (houseHoldItemDataID == -9999) {
+            gameObject.SetActive(false);
+        }
+        else {
+            RegisterOriginalMaterials();
+            MainGameManager.AddObject(this);
+        }
     }
 
     private void OnDisable() {
         MainGameManager.RemoveObject(this);
+    }
+
+
+    private void FixedUpdate() {
+        if(visualMaterial != visualMaterial_was){
+            switch(visualMaterial){
+                case VisualMaterial.normalMaterial:
+                    SetMaterial(null);
+                    break;
+                case VisualMaterial.canPlace:
+                    SetMaterial(canPlaceMaterial);
+                    break;
+                case VisualMaterial.cannotPlace:
+                    SetMaterial(cannotPlaceMaterial);
+                    break;
+            }
+        }
+        visualMaterial_was = visualMaterial;
     }
 
 
@@ -49,5 +87,35 @@ public class HouseHoldItem_monobehaviour : MonoBehaviour {
         return ret;
     }
 
+
+    private void RegisterOriginalMaterials(){
+        originalMaterials.Clear();
+        RegisterOriginalMaterialFor(transform);
+    }
+
+    private void RegisterOriginalMaterialFor(Transform t){
+        var renderers = new List<MeshRenderer>(t.GetComponentsInChildren<MeshRenderer>());
+
+        for(int i=0; i<renderers.Count; i++){
+            originalMaterials.Add(renderers[i], renderers[i].materials);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="m">Original when m == null</param>
+    private void SetMaterial(Material m){
+
+        foreach(var pair in originalMaterials){
+            int l = pair.Key.materials.Length;
+            var mats = new Material[l];
+            for (int i = 0; i < l; i++) {
+                mats[i] = m ?? pair.Value[i];
+            }
+            pair.Key.sharedMaterials = mats;
+        }
+
+    }
 
 }
