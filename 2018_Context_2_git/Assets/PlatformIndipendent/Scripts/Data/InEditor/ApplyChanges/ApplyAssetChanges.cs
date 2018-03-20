a5c689747609d26736b56924f218a074acf34d0b
aWolfKing <31818613+aWolfKing@users.noreply.github.com>
2018-03-20 12:35:50 +0100

    House hold data test

---
+++ 2018_Context_2_git/Assets/PlatformIndipendent/Scripts/Data/InEditor/ApplyChanges/ApplyAssetChanges.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ApplyAssetChanges {
+
+    [MenuItem("Assets/Apply changes")]
+    public static void ApplyChanges(){
+        AssetDatabase.SaveAssets();
+    }    
+
+}
+#endif
